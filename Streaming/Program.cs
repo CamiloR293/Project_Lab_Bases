27249a0f8406c6814f60edc9cc1237bb53dff34f
Parzivak <bherrera@unicauca.edu.co>
2023-06-18 15:33:56 -0500

    act

---
+++ Streaming/Program.cs
@@ -0,0 +1,28 @@
+﻿using Streaming.feedback;
+using Streaming.logica;
+using System;
+using System.Windows.Forms;
+
+namespace Streaming
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Main());
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error al iniciar la aplicacion -> " + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Streaming/logica/cliente_plan.cs
@@ -0,0 +1,29 @@
+﻿using Streaming.connection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Streaming.logica
+{
+    internal class cliente_plan
+    {
+        Datos dt = new Datos();
+
+        public void ingresarRelacionClientePlan(int codigoCliente,int codigoPlan, String plan)
+        {
+            dt.procedureIdEstadoSuscripcion(codigoCliente, codigoPlan,plan);
+        }
+        public DataSet consultarRegistro()
+        {
+            string consulta;
+            consulta = "select * from Registro";
+            DataSet miDs = dt.ejecutarSELECT(consulta);
+            return miDs;
+        }
+    }
+}
+
